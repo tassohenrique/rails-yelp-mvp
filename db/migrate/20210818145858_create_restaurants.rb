9c955f1731cc16350b7baf98d46aa2464f5b7eeb
Tasso Rodrigues <tasso_henrique@hotmail.com>
2021-08-18 15:33:23 -0400

    FOI O DIA INTEIRO

--- db/migrate/20210818145858_create_restaurants.rb
+++ db/migrate/20210818145858_create_restaurants.rb
@@ -2,8 +2,8 @@ class CreateRestaurants < ActiveRecord::Migration[6.0]
   def change
     create_table :restaurants do |t|
       t.string :name
-      t.string :adress
-      t.integer :phone_number
+      t.string :address
+      t.string :phone_number
       t.string :category
 
       t.timestamps
